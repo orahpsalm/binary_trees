e2bde702fa2d6f4b58e68979a2b1b7640751b2d5
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-08-25 13:22:33 +0100

    Create 12-binary_tree_leaves.c

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_leaves - A function which counts the leaves
+* @tree: The tree
+* Return: The number of leaves
+*/
+
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
