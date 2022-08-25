22005555fbbff09306df718c1b8eec33c00ee5a0
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-08-25 13:01:03 +0100

    Update 1-binary_tree_insert_left.c

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,11 +1,12 @@
-include "binary_trees.h"
+#include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - add a node to the left
- * @parent: pointer to the parent
- * @value: value to add
- * Return: pointer to the new node or NULL
+ * binary_tree_insert_left - Add node to the left
+ * @parent: Pointer to the parent
+ * @value: Value to add
+ * Return: (Pointer to new node) or NULL
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL, *tmp = NULL;
@@ -13,11 +14,11 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	if (!parent)
 		return (NULL);
 
-
 	node = malloc(sizeof(binary_tree_t));
 
 	if (!node)
 		return (NULL);
+
 	node->n = value;
 	node->left = NULL;
 	node->right = NULL;
@@ -31,7 +32,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 
 	parent->left = node;
 	node->parent = parent;
-
 
 	return (node);
 }
