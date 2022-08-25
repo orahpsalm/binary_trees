b66a4b5a59fca8e0cdb7ff971d65f9f2534597e0
Orah, Psalm Chiemeñam <psalmorahcle@gmail.com>
2022-08-25 13:43:18 +0100

    Create 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_uncle - Finds the uncle of a node
+ * @node: A pointer to the node whose uncle is to be found
+ * Return: The uncle
+ */
+ 
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (!node || !node->parent || !node->parent->parent)
+		return (NULL);
+
+	if (node->parent->parent->left == node->parent)
+		return (node->parent->parent->right);
+
+	return (node->parent->parent->left);
+}
